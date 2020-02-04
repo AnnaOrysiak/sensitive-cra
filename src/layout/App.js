dd552f1c755064ecb826f3b92860804e31524045
AnnaOrysiak <walachan88@gmail.com>
2020-02-04 22:33:59 +0100

    added change color mode feature

--- src/layout/App.js
+++ src/layout/App.js
@@ -4,24 +4,35 @@ import Header from './Header';
 import Navigation from './Navigation';
 import Page from './Page';
 import Footer from './Footer';
+import '../style/app.css';
 
 class App extends Component {
+  state = {
+    lightMode: false
+  }
+
+  handleColorMode = () => {
+    this.setState((prevState) => ({ lightMode: !prevState.lightMode }))
+  }
+
   render() {
 
     return (
       <Router>
-        <nav className='navigation'>
-          {<Navigation />}
-        </nav>
-        <header className="banner">
-          {<Header />}
-        </header>
-        <main className="mainContainer">
-          {<Page />}
-        </main>
-        <footer className="footer">
-          {<Footer />}
-        </footer>
+        <div className={this.state.lightMode ? 'appContainer light-mode' : 'appContainer'}>
+          <nav className='navigation'>
+            {<Navigation changeColor={this.handleColorMode} />}
+          </nav>
+          <header className="banner">
+            {<Header />}
+          </header>
+          <main className="mainContainer">
+            {<Page />}
+          </main>
+          <footer className="footer">
+            {<Footer />}
+          </footer>
+        </div>
       </Router>
     );
   }
