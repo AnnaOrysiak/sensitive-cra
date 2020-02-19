2ce38b8f3d27a5f994a63f9ca70db13d0ec98913
AnnaOrysiak <walachan88@gmail.com>
2020-02-19 12:29:47 +0100

    fixed errorpage path

--- src/layout/Page.js
+++ src/layout/Page.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Homepage from '../pages/Homepage'
-import Storypage from '../pages/Storypage'
-import Adminpage from '../pages/Adminpage'
+import Homepage from '../pages/Homepage';
+import Storypage from '../pages/Storypage';
+import Adminpage from '../pages/Adminpage';
 import Loginpage from '../pages/Loginpage';
 import Logoutpage from '../pages/Logoutpage';
 import Errorpage from '../pages/Errorpage';
 
 const Page = () => {
-
   return (
     <>
       <Switch>
@@ -17,10 +16,10 @@ const Page = () => {
         <Route path="/sensitive-cra/admin" component={Adminpage} />
         <Route path="/sensitive-cra/login" component={Loginpage} />
         <Route path="/sensitive-cra/logout" component={Logoutpage} />
-        <Route component={Errorpage} />
+        <Route path="/sensitive-cra/**" component={Errorpage} />
       </Switch>
     </>
   );
-}
+};
 
 export default Page;
