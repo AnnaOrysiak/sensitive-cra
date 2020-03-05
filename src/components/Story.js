66ff7e079a943bbc355de764223a0394c93e4efd
AnnaOrysiak <walachan88@gmail.com>
2020-03-05 09:46:14 +0100

    added content sanitization modified some css

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import parse from 'html-react-parser';
+import DOMPurify from 'dompurify';
 import '../style/story.css';
 
-const Story = (props) => {
+const Story = props => {
   const { type, title, chapter_title, content, authors } = props.story;
 
-  const storyAuthors = authors.map((author, index) => <span className="story__author" key={index}>{author}</span>)
+  const storyAuthors = authors.map((author, index) => (
+    <span className="story__author" key={index}>
+      {author}
+    </span>
+  ));
+
+  const purifiedContent = DOMPurify.sanitize(content, {
+    ALLOWED_TAGS: [
+      'p',
+      // 'b',
+      'strong',
+      'em',
+      'u',
+      'del',
+      'code',
+      'ul',
+      'ol',
+      'li',
+      'h5',
+      'h6'
+    ],
+    ALLOWED_ATTR: ['style']
+  });
+  console.log(DOMPurify);
 
   return (
     <article className="story">
-      <small>{type === "story" ? title + ' / ' + chapter_title : `${title} (${type})`}</small>
-      <h3 className="story__title">{type === "story" ? chapter_title : title}</h3>
-      <div className="story__content">{parse(content)}</div>
+      <small>
+        {type === 'story'
+          ? title + ' / ' + chapter_title
+          : `${title} (${type})`}
+      </small>
+      <h3 className="story__title">
+        {type === 'story' ? chapter_title : title}
+      </h3>
+      <div className="story__content">{parse(purifiedContent)}</div>
       <p className="story__authors">{storyAuthors}</p>
     </article>
-
   );
-}
+};
 
 export default Story;
--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const StoryList = (props) => {
-
+const StoryList = props => {
   const { stories, navigationHandler } = props;
   const storiesList = [];
   const shortStories = [];
@@ -11,10 +10,14 @@ const StoryList = (props) => {
     const longStories = [];
     for (let i = 0; i < stories.length; i++) {
       if (i === 0 && stories[i].chapter_title) {
-        longStories.push(stories[i].title)
-      } else if (stories[i].title !== stories[i - 1].title && stories[i].chapter_title) longStories.push(stories[i].title)
+        longStories.push(stories[i].title);
+      } else if (
+        stories[i].title !== stories[i - 1].title &&
+        stories[i].chapter_title
+      )
+        longStories.push(stories[i].title);
       else if (stories[i].title !== stories[i - 1].title) {
-        shortStories.push(stories[i].title)
+        shortStories.push(stories[i].title);
       }
     }
     return longStories;
@@ -22,47 +25,52 @@ const StoryList = (props) => {
 
   function filterTitle(title, storiesList) {
     const chapters = [];
-    storiesList.filter((story) => {
+    storiesList.filter(story => {
       if (story.title === title) {
         chapters.push(
           <li key={story._id}>
-            <NavLink to={`/sensitive-cra/story/${story._id}`} id={story._id} onClick={navigationHandler} className={story.chapter_title ? "storyBox__item" : "storyBox__item storyBox__item--short storyBox__title"}>{story.chapter_title ? story.chapter_title : story.title}</NavLink>
+            <NavLink
+              to={`/sensitive-cra/story/${story._id}`}
+              id={story._id}
+              onClick={navigationHandler}
+              className={
+                story.chapter_title
+                  ? 'storyBox__item'
+                  : 'storyBox__item storyBox__item--short storyBox__title'
+              }
+            >
+              {story.chapter_title ? story.chapter_title : story.title}
+            </NavLink>
           </li>
-        )
+        );
       }
-      return story.title === title
-    })
+      return story.title === title;
+    });
     return chapters;
   }
-
 
   getStoriesTitlesList().forEach((item, id) => {
     storiesList.push(
       <li key={item + id} className="storyBox storyBox--long">
         <h3 className="storyBox__title">{item}</h3>
-        <ul className="storyBox__list">
-          {filterTitle(item, stories)}
-        </ul>
-      </li>)
-  })
+        <ul className="storyBox__list">{filterTitle(item, stories)}</ul>
+      </li>
+    );
+  });
 
   shortStories.forEach((item, id) => {
     storiesList.push(
       <li className="storyBox" key={item + id}>
-        <h3>
-          {filterTitle(item, stories)}
-        </h3>
-      </li>)
-  })
-
+        <h3>{filterTitle(item, stories)}</h3>
+      </li>
+    );
+  });
 
   return (
     <>
-      <ul>
-        {storiesList}
-      </ul>
+      <ul className="storiesBox">{storiesList}</ul>
     </>
   );
-}
+};
 
 export default StoryList;
